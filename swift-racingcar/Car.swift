34bc40c51b9207f13b10660f42cd29c1e7e8419e
ellyheetov <yaaajcm@naver.com>
2020-12-05 12:48:54 +0900

    add skeleton code

---
+++ swift-racingcar/Car.swift
@@ -0,0 +1,17 @@
+//
+//  Car.swift
+//  swift-racingcar
+//
+//  Created by 박혜원 on 2020/12/04.
+//
+
+import Foundation
+
+struct Car {
+    private var name : String
+    private var position: Int = 0
+    
+    init(name : String){
+        self.name = name
+    }
+}
